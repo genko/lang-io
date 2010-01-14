40230bc97a24e1199b438b607e0a8784d0d642f0
David Schneider <david.schneider@picle.org>
2010-01-14 10:28:06 +0000

    Initial straight forward port of the io operator shuffling code, not yet completely working

--- io/objspace.py
+++ io/objspace.py
@@ -200,6 +200,8 @@ class ObjSpace(object):
             return self.w_true
         return self.w_false
     
+    def newsequence(self, value):
+        return self.w_immutable_sequence.clone_and_init(value)
     def isnil(self, w_object):
         return w_object is self.w_nil
         
